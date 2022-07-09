b024bc16acb0fdc244714f18188da083afdadf15
Yeison <enmanuelquiterios@gmail.com>
2022-07-09 12:49:58 -0400

    Solved and created a server class to reduce code in the app initialization

--- NodeJS-REST-API/app.js
+++ NodeJS-REST-API/app.js
@@ -1,41 +1,5 @@
-const express = require("express");
-const bodyParser = require("body-parser");
 require("dotenv").config();
-const { dbConnection } = require("./database/config");
-// Initializing instaces of My dependancies
+const Server = require("./models/server");
+const server = new Server();
 
-const app = express();
-// Middleware to parse the data
-app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-
-//  Importing routes into App Js
-
-const feedRoutes = require("./routes/feed");
-
-// Solving CORS setting Headers PLEASE THIS IS SO IMPORTANT
-
-app.use((req, res, next) => {
-  // This allow my server to receive request from all different domains or local Host
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // This allow my server to receive thos method only
-  res.setHeader(
-    "Acess-controll-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH"
-  );
-  // Those are the Header type from the Client and Authorization allowing the server to set content type: Content-Type : "application/json"
-  res.setHeader("Access-Control-Allow-Header", "Content-Type, Authorization");
-  next();
-});
-
-//
-
-app.use("/feed", feedRoutes);
-
-app.listen(
-  3002,
-  (connectarDB = async () => {
-    await dbConnection();
-  })
-);
+server.listen();
--- NodeJS-REST-API/models/server.js
+++ NodeJS-REST-API/models/server.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const { dbConnection } = require("../database/config");
+const bodyParser = require("body-parser");
+
 const feedRoutes = require("../routes/feed");
 
 class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
+    this.postUser = feedRoutes;
+
+    // Connecting and calling methods
+    this.connectarDB();
+    this.middlewares();
+    this.routes();
+    this.parserBody();
   }
+
+  // Calling meethods
 
   // Here I create my function and connected my database
   connectarDB = async () => {
@@ -22,4 +33,20 @@ class Server {
     // Directorio Público
     this.app.use(express.static("public"));
   }
+
+  routes() {
+    this.app.use(this.postUser);
+  }
+
+  parserBody() {
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Server is running on port: ", this.port);
+    });
+  }
 }
+
+module.exports = Server;
